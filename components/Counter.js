644fdcfeb2ae6641a045ab02650a19f1dbeb89fe
Sola <so.ogunsola@gmail.com>
2022-12-09 21:28:25 -0600

    Build counter component

---
+++ components/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+function Counter({ count, title }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        {title} {count}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 45,
+  },
+});
+
+export default Counter;
